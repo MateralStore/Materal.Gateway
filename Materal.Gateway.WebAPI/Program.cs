7d15ec29f429675f6447f14099060bc058bc2c44
Materal <cloomcmx1554@hotmail.com>
2025-07-24 14:24:18 +0800

    feat: 添加项目文件

---
+++ Materal.Gateway.WebAPI/Program.cs
@@ -0,0 +1,9 @@
+using Materal.MergeBlock.Web;
+
+namespace Materal.Gateway.WebAPI
+{
+    public class Program
+    {
+        public static async Task Main(string[] args) => await MergeBlockProgram.RunAsync(args);
+    }
+}
---
+++ Materal.Gateway/Middleware/OcelotMiddlewareExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Ocelot.Middleware;
+
+namespace Materal.Gateway.Middleware
+{
+    /// <summary>
+    /// 网关中间件扩展
+    /// </summary>
+    public static class GatewayMiddlewareExtensions
+    {
+        /// <summary>
+        /// 使用网关
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="gatewayInterception"></param>
+        /// <returns></returns>
+        public static async Task<IApplicationBuilder> UseGateway(this IApplicationBuilder builder, bool gatewayInterception = false)
+        {
+            builder.UseWebSockets();
+            await builder.UseOcelot((builder, config) => builder.UseGatewayPipeline(config, gatewayInterception));
+            return builder;
+        }
+    }
+}
