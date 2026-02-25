d13841a767c7d0a93dd0a7d745a3a6033a14c806
Materal <cloomcmx1554@hotmail.com>
2026-02-25 11:20:58 +0800

    feat: 移除旧项目

--- Materal.Gateway/GatewayException.cs
+++ Materal.Gateway/GatewayException.cs
@@ -1,24 +1,23 @@
-﻿namespace Materal.Gateway
+﻿namespace Materal.Gateway;
+
+/// <summary>
+/// 网关异常
+/// </summary>
+public class GatewayException : MateralException
 {
     /// <summary>
-    /// 网关异常
+    /// 构造方法
     /// </summary>
-    public class GatewayException : MateralException
-    {
-        /// <summary>
-        /// 构造方法
-        /// </summary>
-        public GatewayException() { }
-        /// <summary>
-        /// 构造方法
-        /// </summary>
-        /// <param name="message"></param>
-        public GatewayException(string message) : base(message) { }
-        /// <summary>
-        /// 构造方法
-        /// </summary>
-        /// <param name="message"></param>
-        /// <param name="innerException"></param>
-        public GatewayException(string message, Exception innerException) : base(message, innerException) { }
-    }
+    public GatewayException() { }
+    /// <summary>
+    /// 构造方法
+    /// </summary>
+    /// <param name="message"></param>
+    public GatewayException(string message) : base(message) { }
+    /// <summary>
+    /// 构造方法
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public GatewayException(string message, Exception innerException) : base(message, innerException) { }
 }
