dc4d6bee0d51b0b8b3d937182906cd4d5ef3911d
Materal <cloomcmx1554@hotmail.com>
2026-02-26 09:29:48 +0800

    feat: 更改打包路径

--- MMB.Test.Application/TestModule.cs
+++ MMB.Test.Application/TestModule.cs
@@ -1,8 +1,17 @@
-﻿namespace Materal.Gateway.Application;
+﻿using MMB.Test.Application.Hubs;
+
+namespace Materal.Gateway.Application;
 
 /// <summary>
 /// 测试模块
 /// </summary>
 public partial class TestModule() : MergeBlockModule("网关模块")
 {
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        base.OnApplicationInitialization(context);
+        AdvancedContext advancedContext = context.ServiceProvider.GetRequiredService<AdvancedContext>();
+        if (advancedContext.App is not WebApplication webApplication) return;
+        webApplication.MapHub<TestHub>("/hubs/test");
+    }
 }
